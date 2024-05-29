dd2234d0a3fe780f3092d8ac71e45e636fd19716
NguyenNgan03 <simple.270123@gmail.com>
2024-05-29 14:33:01 +0700

    About

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 const App = () => {
-const [users, setUsers] = useState([]);
-useEffect(() => {
-const fetchData = async () => {
-try {
-  const response = await axios.get("https://api.github.com/users");
-  console.log(response);
-  setUsers(response.data);
-} catch (error) {
-console.error("Error fetching data:", error);
-}
-};
-fetchData();
-}, []);
-return (
-  <div className="App">
-    <Navbar />
-    <div className="container">
-      <h1>GitHub Users Data</h1>
-      <Search />
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        console.log(response);
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" components={Search} />
+          </Switch>
+        </div>
+        <Users users={users} />
+      </Router>
     </div>
-    <Users users={users} />
-  </div>
-);
+  );
 };
 export default App;
